9b4b771b86976cf5a1d759e7badb77cec9133408
Mani Monajjemi <mani.monajjemi@gmail.com>
2012-09-23 16:16:57 -0700

    committing all changes, equals to git add and git commit

--- librapiautolabtracker/autolabtracker.cpp
+++ librapiautolabtracker/autolabtracker.cpp
@@ -108,7 +108,8 @@ void CAutolabTracker::updateData( const double dt )
  void CAutolabTracker::writeToDataBase(const double dt, std::string robotName, std::string value)
 {
 	std::string key = "robot"+robotName;
-    	if (mRedisClient->set(key , value.c_str() ) == 0) 
+	printf("key : %s, value : %s\n",key.c_str(),value.c_str());
+    	if (mRedisClient->set(key.c_str() , value.c_str() ) == 0) 
 	{
       			PRT_WARN0("Failed to write position information from tracker");
       			return;
